64af471ea3980e1638ee616c89f348a23788d7ab
Aleksandr Belousov <lksndrbelousov1983@mail.ru>
2023-10-18 15:43:21 +0700

    Second commit

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,10 +1,11 @@
 public class Post {
+    public FormDate birthday;
     public String name;
     public String passport;
     public String patronymic;
     public String phone;
     public String surname;
-    public String subscription;
-    public FormDate birthday;
+    public boolean subscription;
+
 }
 
